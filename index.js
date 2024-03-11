3cbc7375779df7919b8662388b805d30e0604854
Pedro Alarcon <pedroa54@icloud.com>
2024-03-11 17:27:15 -0500

    adds complete lab

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your code in this file!
-const currentUser = 'Grace Hopper';
+const currentUser = 'Pedro';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//!const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
